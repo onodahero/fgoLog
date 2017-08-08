22434982bf111992a9d523fa5a3a1797d4132444
斧田洋人 <hiroto@s-hiroto-onoda-no-macbook-pro.local>
2017-08-08 11:34:40 +0900

    Initial Commit

---
+++ fgoLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fgoLog
+//
+//  Created by 斧田洋人 on 2017/08/08.
+//  Copyright © 2017年 斧田洋人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
